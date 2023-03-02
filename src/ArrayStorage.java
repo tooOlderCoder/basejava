730c604ce197c9cabbe0bab76450beeac1613d28
Niki <nstulikov@bk.ru>
2023-03-02 15:14:08 +0500

    HW0 change2

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -29,8 +29,6 @@ public class ArrayStorage {
         for (int i = 0; i < size; i++) {
             if (storage[i].toString().equals(uuid)) {
                 return storage[i];
-            } else {
-                return null;
             }
         }
         return null;
